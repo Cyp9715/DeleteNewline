a9dfdd4b2afe465eeea22e77b7b285a527f49411
ByoungGyu Lee <Cypher9715@naver.com>
2023-04-16 17:25:52 +0900

    초기 커밋.

---
+++ RemovePdfNewline/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace RemovePdfNewline
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyboardDevice.Modifiers == ModifierKeys.Control && e.Key == Key.V)
+            {
+                string text = TextBox_Main.Text;
+                string removeNewline = text.Replace("\r\n", " ");
+                Clipboard.SetDataObject(removeNewline);
+            }
+        }
+    }
+}
