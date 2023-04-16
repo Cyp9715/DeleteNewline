f90c9de526c6a40b8842897fa14671415496dc5c
ByoungGyu Lee <Cypher9715@naver.com>
2023-04-16 22:18:16 +0900

    1. 프로젝트 명을 변경하였습니다. 2. TopMost 기능을 추가하였습니다. 3. 마우스 우클릭으로 붙여넣기 기능을 수정하였습니다. 4. Text 형태가 아닌 붙여넣기를 수행할 시, 경고메세지를 띄우고 작업이 정상적으로 이뤄지지 않게 수정하였습니다.

---
+++ DeleteNewline/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace DeleteNewline
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainWindow mainWindow;
+        IDataObject idata;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainWindow = (MainWindow)Application.Current.MainWindow;
+        }
+
+        private bool CheckDataForm()
+        {
+            idata = Clipboard.GetDataObject();
+            if (idata.GetDataPresent(DataFormats.Text) == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void DeleteNewline()
+        {
+            if (CheckDataForm() == false)
+            {
+                AddAlertMsg();
+                return;
+            }
+
+            string text = (string)idata.GetData(DataFormats.Text);
+            string deleteNewline = text.Replace("\r\n", " ");
+            Clipboard.SetDataObject(deleteNewline);
+        }
+
+        private void AddAlertMsg()
+        {
+            TextBox_Main.AppendText("\r\n");
+            TextBox_Main.AppendText(" ========================================================\r\n");
+            TextBox_Main.AppendText(" ================== Only text form can be entered =================\r\n");
+            TextBox_Main.AppendText(" ========================================================\r\n");
+        }
+
+
+        private void TextBox_Main_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftCtrl) && e.Key == Key.V || Keyboard.IsKeyDown(Key.V) && e.Key == Key.LeftCtrl)
+            {
+                DeleteNewline();
+            }
+        }
+
+        private void MenuItem_TopMost_Checked(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Topmost = true;
+        }
+
+        private void MenuItem_TopMost_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Topmost = false;
+        }
+
+        private void MenuItem_Paste_Click(object sender, RoutedEventArgs e)
+        {
+            // To add text to textbox.
+            if(CheckDataForm() == false)
+            {
+                AddAlertMsg();
+                return;
+            }
+
+            TextBox_Main.AppendText((string)idata.GetData(DataFormats.Text));
+            DeleteNewline();
+        }
+    }
+}
