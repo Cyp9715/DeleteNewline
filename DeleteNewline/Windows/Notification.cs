ec0cc46854ca057141be1ffebc2160d420245af8
ByoungGyu Lee <Cypher9715@naver.com>
2023-05-03 12:10:51 +0900

    실질적으로 DeleteNewline 의 Notification 은 입력처리가 성공적으로 이뤄졌다는것을 사용자에게 알려주는 목적이 있지, 나중에 누적된 알림을 확인하기 위한 목적이 아닙니다.
    때문에 Notification expirationTime 을 기본적으로 30초로 설정하여 20초 뒤에는 생성된 알림이 지워지도록 하였습니다.
    
    만약 해당 옵션이 완전히 필요 없다고 생각될 경우 생성즉시 알림을 지우는 방안 역시 고려중입니다.

--- DeleteNewline/Windows/Notification.cs
+++ DeleteNewline/Windows/Notification.cs
@@ -1,19 +1,25 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
+using Windows.UI.Notifications;
 
 namespace Windows
 {
     static class Notification
     {
-        public static void Send(string title, string content)
+        public static void Send(string title, string content, int expirationTime = 30)
         {
             Task task_notify = new Task(() =>
             {
                 // Requires Microsoft.Toolkit.Uwp.Notifications NuGet package version 7.0 or greater
                 new ToastContentBuilder()
-                    .AddText(title)
-                    .AddText(content)
-                    .Show();
+                .AddText(title)
+                .AddText(content)
+                .Show(toast =>
+                {
+                    toast.ExpirationTime = DateTime.Now.AddSeconds(expirationTime);
+                });
             });
 
             task_notify.Start();
