2d13d72a9a7ce501bbbb825df6aee1cfc48ac86b
ByoungGyu Lee <Cypher9715@naver.com>
2023-05-21 00:03:50 +0900

    1.2.3 버전 업데이트.

--- DeleteNewline/Settings.Designer.cs
+++ DeleteNewline/Settings.Designer.cs
@@ -73,7 +73,7 @@ namespace DeleteNewline {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool notification {
             get {
                 return ((bool)(this["notification"]));
