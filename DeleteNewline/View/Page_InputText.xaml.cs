f8ea7f147fa80c7e2c1aa823b42376ac6747b1fe
ByoungGyu Lee <Cypher9715@naver.com>
2023-06-17 00:31:34 +0900

    update input text error

--- DeleteNewline/View/Page_InputText.xaml.cs
+++ DeleteNewline/View/Page_InputText.xaml.cs
@@ -9,7 +9,6 @@ namespace DeleteNewline
     {
         public static Page_InputText? instance;
         ViewModel_Page_InputText vm_Input;
-        ViewModel_Page_Setting settings = ViewModel_Page_Setting.page_settings;
 
         public Page_InputText()
         {
@@ -30,8 +29,11 @@ namespace DeleteNewline
             {
                 originalText = (string)idataObj.GetData(DataFormats.Text);
                 TextBox_Main.AppendText(originalText);
-                Clipboard.SetDataObject(clipboardManager.applyRegex(ref idataObj,
-                        settings.text_textBox_regexExpression, settings.text_textBox_regexReplace).ToString());
+
+                (var success, var replaceText) = clipboardManager.applyRegex(ref idataObj,
+                        ViewModel_Page_Setting.page_settings.text_textBox_regexExpression, 
+                        ViewModel_Page_Setting.page_settings.text_textBox_regexReplace);
+                Clipboard.SetDataObject(replaceText);
             }
             else
             {
@@ -46,8 +48,11 @@ namespace DeleteNewline
             {
                 if (clipboardManager.GetClipboardText(ref idataObj) == true)
                 {
-                    Clipboard.SetDataObject(clipboardManager.applyRegex(ref idataObj,
-                            settings.text_textBox_regexExpression, settings.text_textBox_regexReplace).ToString());
+                    (var success, var replaceText) = clipboardManager.applyRegex(ref idataObj,
+                            ViewModel_Page_Setting.page_settings.text_textBox_regexExpression,
+                            ViewModel_Page_Setting.page_settings.text_textBox_regexReplace);
+
+                    Clipboard.SetDataObject(replaceText);
                 }
                 else
                 {
