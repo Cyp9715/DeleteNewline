c4d20851865e84deca947d6c806c1b63e1e571b1
ByoungGyu Lee <Cypher9715@naver.com>
2023-06-27 06:09:04 +0900

    add comments.

--- DeleteNewline/Windows/Startup.cs
+++ DeleteNewline/Windows/Startup.cs
@@ -2,6 +2,11 @@
 
 namespace Windows
 {
+    /* 
+     * 해당 코드는 고전적인 .exe 파일에서만 적용할 수 있습니다.
+     * 명시적인 저장공간이 제공되지 않는 MSIX 파일에서는 적용할 수 없는 코드입니다.
+     * 해당 코드는 MSIX 로 제공되는 DeleteNewline 특성상 실제로 사용되지 않습니다.
+     */
     static class Startup
     {
         public static Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
