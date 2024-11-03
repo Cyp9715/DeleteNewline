a8706fd659d2082d4fddd5a7368c68140c1ac686
ByoungGyu Lee <Cypher9715@naver.com>
2024-11-04 05:22:45 +0900

    added discard

--- Delete Newline/ViewModels/KeybindCollectViewModel.cs
+++ Delete Newline/ViewModels/KeybindCollectViewModel.cs
@@ -24,7 +24,7 @@ public partial class KeybindCollectViewModel : ObservableRecipient
     [RelayCommand]
     public void AddKeybind()
     {
-        _keybindCollectManagerService.AddRegexChain();
+        _ = _keybindCollectManagerService.AddRegexChain();
     }
 
     [RelayCommand]
@@ -32,7 +32,7 @@ public partial class KeybindCollectViewModel : ObservableRecipient
     {
         if (chain is not null)
         {
-            _keybindCollectManagerService.RemoveRegexChain(chain);
+            _ = _keybindCollectManagerService.RemoveRegexChain(chain);
         }
     }
 }
