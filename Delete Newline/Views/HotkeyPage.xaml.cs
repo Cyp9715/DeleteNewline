8df6140b97124ce1cc29d6323a1b384d031111f1
ByoungGyu Lee <Cypher9715@naver.com>
2025-02-16 22:27:53 +0900

    Configured HotkeyPage so that pressing the mouse's "Back" button navigates back to HotkeyCollectPage.
    A global back/forward navigation scheme was not implemented throughout the application to avoid potentially degrading usability. Instead, this functionality has been added in a limited fashion solely to ease the inconvenience of switching between HotkeyPage and HotkeyCollectPage.

--- Delete Newline/Views/HotkeyPage.xaml.cs
+++ Delete Newline/Views/HotkeyPage.xaml.cs
@@ -1,5 +1,7 @@
 using Delete_Newline.ViewModels;
+using Microsoft.UI.Input;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 
 namespace Delete_Newline.Views;
 
@@ -15,5 +17,18 @@ public sealed partial class HotKeyPage : Page
         InitializeComponent();
         ViewModel = App.GetService<HotKeyViewModel>();
         DataContext = ViewModel;
+    }
+
+    private void HotKeyPage_PointerPressed(object sender, PointerRoutedEventArgs e)
+    {
+        // 현재 포인터 이벤트 정보를 가져옵니다.
+        var point = e.GetCurrentPoint(this);
+
+        // XButton1Pressed는 보통 마우스의 '뒤로가기' 버튼입니다.
+        if (point.Properties.PointerUpdateKind == PointerUpdateKind.XButton1Pressed)
+        {
+            e.Handled = true;
+            Frame.Navigate(typeof(HotKeyCollectPage));
+        }
     }
 }
