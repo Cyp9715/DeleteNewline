1c0fbfe0c606daf59acf512352d13aea0d96c34f
ByoungGyu Lee <Cypher9715@naver.com>
2025-03-03 21:38:12 +0900

    Modify ToObjectAsync<T>, StringifAsync()

--- Delete Newline.Core/Helpers/Json.cs
+++ Delete Newline.Core/Helpers/Json.cs
@@ -1,13 +1,12 @@
 ﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Delete_Newline.Core.Helpers;
 
 public static class Json
 {
-    public static async Task<T> ToObjectAsync<T>(string value, JsonSerializerSettings? settings = null)
+    public static Task<T> DeserializeAsync<T>(string value, JsonSerializerSettings? settings = null)
     {
-        return await Task.Run(() =>
+        return Task.Run(() =>
         {
             settings ??= new JsonSerializerSettings
             {
@@ -20,7 +19,7 @@ public static class Json
         });
     }
 
-    public static async Task<string> StringifyAsync(object value, JsonSerializerSettings? settings = null)
+    public static Task<string> SerializeAsync(object value, JsonSerializerSettings? settings = null)
     {
         settings ??= new JsonSerializerSettings
         {
@@ -28,20 +27,10 @@ public static class Json
             Formatting = Formatting.Indented,
         };
 
-        /*
-         * The reason for performing a DeepCopy in this code is due to the repeated calls 
-         * to Add and Remove on the regexChains variable during the Drag&Drop swap between GridView items. 
-         * This causes a System.InvalidOperationException: 'Collection was modified; enumeration operation may not execute.' error.
-         * While it is possible to indirectly ensure thread safety by using await Task.Delay(100), I judged this to be a temporary workaround. 
-         * 
-         * Therefore, I decided to use DeepCopy, even though it consumes more resources.
-         * However, this is not the best solution either. 
-         * The optimal solution would be to link the Swap method during the Drag&Drop operation in the GridView.
-         */
 
         var deepCopy = JsonConvert.DeserializeObject<object>(JsonConvert.SerializeObject(value, settings), settings);
 
-        return await Task.Run(() =>
+        return Task.Run(() =>
         {
             return JsonConvert.SerializeObject(deepCopy, settings);
         });
