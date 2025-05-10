3bad19fa4a6ffa147e89793d4ab5241abdbbb9f5
ByoungGyu Lee <Cypher9715@naver.com>
2025-05-10 23:25:32 +0900

    changing it to ObservableObject due to the need to store the inherited IsActive field.

--- Delete Newline/Contracts/Structures/HotkeyStructure.cs
+++ Delete Newline/Contracts/Structures/HotkeyStructure.cs
@@ -1,25 +1,16 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using Delete_Newline.Helpers;
-using System.Text.Json.Serialization;
 using Windows.System;
-using System.Runtime.InteropServices;
 
 namespace Delete_Newline.Contracts.Structures;
 
-public partial class HotkeyStructure : ObservableRecipient
+public partial class HotkeyStructure : ObservableObject
 {
     [ObservableProperty]
     public VirtualKeyModifiers _modifiers;
 
     [ObservableProperty]
     public VirtualKey _key;
-
-    [JsonIgnore]
-    public new bool IsActive
-    {
-        get => base.IsActive;
-        set => base.IsActive = value;
-    }
 
     public HotkeyStructure()
     {
